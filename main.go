0e639bf61c528115ebb5423dfaea927671c1e270
Max-Cheng <aaron9shire@gmail.com>
2020-10-27 15:50:03 +0800

    add:conf

--- main.go
+++ main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"MessageBoard/common"
+	"MessageBoard/conf"
+
 	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
 	common.InitDB()
-	r:=gin.Default()
+	r := gin.Default()
 	if mode := gin.Mode(); mode == gin.TestMode {
 		r.LoadHTMLGlob("./../templates/*")
 	} else {
 		r.LoadHTMLGlob("templates/*")
 	}
-	r.Static("/statics","./statics")
+	r.Static("/statics", "./statics")
 	r = CollectRoute(r)
-	r.Run(":6789")
+	r.Run(":" + conf.Accessport)
 }
 
 func init() {
 	gin.SetMode(gin.DebugMode)
 }
-
